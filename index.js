f5b7e535d550c80d4f001b539f29f7cbeb40a9d0
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-06-05 10:36:19 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_KEY}@cluster0.d2cwisz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
